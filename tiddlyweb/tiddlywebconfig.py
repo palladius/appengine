3eee484d37a3533dd6647ecf04d9b8e6c1336582
Riccardo GCarlesso <riccardo.carlesso@gmail.com>
2012-09-14 15:01:56 +0100

    just drafted

---
+++ tiddlyweb/tiddlywebconfig.py
@@ -0,0 +1,8 @@
+# A basic configuration.
+# Run "pydoc tiddlyweb.config" for details on configuration items.
+
+config = {
+    'system_plugins': ['tiddlywebwiki'],
+    'secret': '65b50ae3f4907404ca66e06838f6aded3f2e7281',
+    'twanager_plugins': ['tiddlywebwiki'],
+}
