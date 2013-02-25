834d79924599e802005ed733a2d48305ad5979b5
Riccardo Carlesso <palladiusbonton+git@gmail.com>
2013-02-25 21:08:02 +0100

    v3 jinja lets test it

--- provapally.appspot.com/helloworld.py
+++ provapally.appspot.com/helloworld.py
@@ -1,40 +1,38 @@
 
 # https://developers.google.com/appengine/docs/python/gettingstarted/usingusers
 
-from google.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
 
-import jinja2
-import os
+# 2.5 obsolete
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(
-        os.path.dirname(__file__)))
+# from google.appengine.api import users
+# from google.appengine.ext import webapp
+# from google.appengine.ext.webapp.util import run_wsgi_app
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
+# import jinja2
+# import os
 
-        if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('[ProvaPally]: Hello, ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+# jinja_environment = jinja2.Environment(
+#     loader=jinja2.FileSystemLoader(
+#         os.path.dirname(__file__)))
 
-application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
-                                     debug=True)
+# class MainPage(webapp.RequestHandler):
+#     def get(self):
+#         user = users.get_current_user()
+#         if user:
+#             self.response.headers['Content-Type'] = 'text/plain'
+#             self.response.out.write('[ProvaPally Obsolete]: Hello, ' + user.nickname())
+#         else:
+#             self.redirect(users.create_login_url(self.request.uri))
 
+# application = webapp.WSGIApplication( [('/', MainPage)], debug=True )
 
-def main():
-    #run_wsgi_app(application)
-    template = jinja_environment.get_template(
-    'index.html')
-    self.response.write(template.render())
+# def main():
+#     #run_wsgi_app(application)
+#     template = jinja_environment.get_template('index.html')
+#     self.response.write(template.render())
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 
--- provapally.appspot.com/main.py
+++ provapally.appspot.com/main.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 
+# 2.7
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
 import webapp2
+import jinja2
+import os
 
-class MainHandler(webapp2.RequestHandler):
+#class MainHandler(webapp2.RequestHandler):
+#    def get(self):
+#        self.response.out.write('Hello world (Riccardo @ Provapally)!')
+
+#app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+
+class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world (Riccardo @ Provapally)!')
+        user = users.get_current_user()
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write('[ProvaPally Obsolete]: Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
-app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+app = webapp.WSGIApplication( [('/', MainPage)], debug=True )
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(
+        os.path.dirname(__file__)))
